47783b3dcb00ac219a76def99632c7cd32d0eab6
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2024-10-06 16:36:32 +0200

    initial

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <poll.h>
+#include <errno.h>
+
+void printValue(char* constName, const long constValue) {
+  printf("%s, decimal=%ld, unsigned long hex=0x%04lx, unsigned int hex=0x%04x\n",
+    constName, constValue, constValue, (int)constValue);
+}
+
+int main(int argc, char *argv[]) {
+  printValue("O_WRONLY", O_WRONLY); // fcntl.h
+  printValue("O_RDWR", O_RDWR); // fcntl.h
+  printValue("POLLIN", POLLIN); // poll.h
+  printValue("EAGAIN", EAGAIN); // errno.h
+  printValue("EINTR", EINTR); // errno.h
+  printValue("O_NOCTTY", O_NOCTTY); // fcntl.h
+  return 0;
+}
